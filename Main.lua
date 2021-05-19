8791d6bca86a1d4351a9da472c148138a85e38ba
Maczuga <maczugapl@gmail.com>
2021-05-19 19:17:26 +0200

    Support sections

--- Main.lua
+++ Main.lua
@@ -1,9 +1,39 @@
+local version = select(4, GetBuildInfo());
+
+local tiers = {
+  -- Cata
+  ds = {1, 2},
+  -- MoP
+  msv = {3, 4},
+  hof = {5, 6},
+  toes = {7},
+  tot = {8, 9, 10, 11},
+  soo = {12, 13, 14, 15},
+};
+
 SLASH_LFR_AUTOQ1 = "/alfr"
 
+function QueueSection(i)
+  local pLvl = UnitLevel("player")
+
+  local id, name, _, _, min, max = GetRFDungeonInfo(i)
+  local total, killed = GetLFGDungeonNumEncounters(id)
+
+  if not (pLvl < min or pLvl > max) then
+      if killed ~= total and IsLFGDungeonJoinable(id) then
+        print("Queueing to: " .. name)
+        SetLFGDungeon(LE_LFG_CATEGORY_LFR, id)
+      end
+  end
+end
+
 SlashCmdList["LFR_AUTOQ"] = function(arg)
+  -- Roles
   local asTank = string.find(arg, "tank")
   local asHeal = string.find(arg, "heal")
   local asDPS = string.find(arg, "dps")
+
+  arg = " " .. arg .. " ";
 
   if string.find(arg, "all") then
     asTank = true
@@ -11,19 +41,22 @@ SlashCmdList["LFR_AUTOQ"] = function(arg)
     asDPS = true
   end
 
-  local pLvl = UnitLevel("player")
 
   ClearAllLFGDungeons(LE_LFG_CATEGORY_LFR)
   SetLFGRoles(LE_LFG_CATEGORY_LFR, asTank, asHeal, asDPS)
 
-  for i = 1, GetNumRFDungeons() do
-    local id, _, _, _, min, max = GetRFDungeonInfo(i)
-    local total, killed = GetLFGDungeonNumEncounters(id)
-
-    if not (pLvl < min or pLvl > max) then
-        if killed ~= total and IsLFGDungeonJoinable(id) then
-          SetLFGDungeon(LE_LFG_CATEGORY_LFR, id)
+  -- Sections
+  if string.find(arg, "full") then
+    for i = 1, GetNumRFDungeons() do
+      QueueSection(i)
+    end
+  else
+    for key, value in pairs(tiers) do
+      if string.find(arg, " " .. key .. " ") then
+        for i = 1, #value do
+          QueueSection(value[i])
         end
+      end
     end
   end
 
--- README.md
+++ README.md
@@ -12,3 +12,14 @@ You can also use/combine spec types:
 
 `/alfr dps tank heal` (= `/alfr all`)
 
+
+You can also use sections:
+- msv
+- hof
+- toes
+- tot
+- soo
+
+Sections can be combined like roles. To queue for MSV, HoF, ToT and SoO (for example you don't wanna queue for ToES, because you wanna complete The Fear Itself quest soon) as all available roles:
+
+`alfr all msv hof tot soo` (order is whatever you want)
